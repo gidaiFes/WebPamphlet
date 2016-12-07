eba5c54a777a10c5426c56e106d523df12fcf034
gainings <gain.beats@gmail.com>
2016-12-08 02:48:28 +0900

    Separate button.jsx ,Add sass for nav

---
+++ src/scripts/views/button.jsx
@@ -0,0 +1,12 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+var Button = React.createClass({
+  render:function(){
+    return(
+      <div className="button">button</div>
+		);
+	}
+});
+
+module.exports = Button;
