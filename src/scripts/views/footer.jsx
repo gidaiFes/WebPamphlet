9aec2fbeceb3fbecd4aab9dd58cfb1aa8140b7be
gainings <gain.beats@gmail.com>
2016-12-08 11:29:37 +0900

    Remove footer text

--- src/scripts/views/footer.jsx
+++ src/scripts/views/footer.jsx
@@ -4,8 +4,6 @@ var Footer = React.createClass({
 	render: function(){
 		return(
       <footer>
-				<p>©技大祭実行委員会 公式WEB SITE 2017</p>
-			  <p>〒940-2188 新潟県長岡市上富岡町 1603TEL:0258-47-9253 (学生係)  MAIL:nutfes.contact@gmail.com</p>
 			</footer>
 		);
 	}
