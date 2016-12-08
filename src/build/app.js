d453ee23be7b2bf957171add63592f86b6d444df
gainings <gain.beats@gmail.com>
2016-12-08 11:26:44 +0900

    Add flag in button,Tweak design

--- src/build/app.js
+++ src/build/app.js
@@ -20614,9 +20614,21 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 
 var Button = React.createClass({displayName: "Button",
+  componentDidMount:function(){
+    this.setState({push_flag: false});
+		this.setState({style: {
+			background: "#FF0000"
+		}});
+  },
+	changeFlag:function(){
+		this.state.push_flag = !this.state.push_flag;
+		console.log("this is %s",this.state.push_flag);
+	},
   render:function(){
     return(
-      React.createElement("div", {className: "button"}, "button")
+      React.createElement("div", null, 
+			  React.createElement("button", {	type: "button", onClick: this.changeFlag}, "button")
+			)
 		);
 	}
 });
--- src/scripts/views/button.jsx
+++ src/scripts/views/button.jsx
@@ -2,9 +2,21 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 
 var Button = React.createClass({
+  componentDidMount:function(){
+    this.setState({push_flag: false});
+		this.setState({style: {
+			background: "#FF0000"
+		}});
+  },
+	changeFlag:function(){
+		this.state.push_flag = !this.state.push_flag;
+		console.log("this is %s",this.state.push_flag);
+	},
   render:function(){
     return(
-      <div className="button">button</div>
+      <div>
+			  <button	type="button" onClick={this.changeFlag}>button</button>
+			</div>
 		);
 	}
 });
